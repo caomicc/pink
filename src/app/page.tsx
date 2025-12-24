d6b914d19c782d5897d262468b5d5622b2c8ab49
Cameron Omiccioli <caomicc@gmail.com>
2025-12-23 20:33:28 -0500

    refactor: update Home component to include additional project focus message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h2>My name is Cam and I build websites.</h2>
       <p>I am the developer of <Link href="https://polishedcrystal.app" target="_blank">PolishedDex</Link>, a fan project for reference data based on the game Polished Crystal.</p>
       <p>I'm not looking for work right now, but I am available for friendly conversation :)</p>
+      <p>Eventually I will add more to this site, but right now I am more focused on other projects. One of those things where I would love to do more here, but will I have the time and energy? Who knows.</p>
     </>
   );
 }
