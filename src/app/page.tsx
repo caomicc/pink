d2f8632ee785a970b017a7e14a6b25351b354c92
Cameron Omiccioli <caomicc@gmail.com>
2026-02-27 19:46:22 -0500

    refactor: enhance layout and typography in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,38 +3,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="mx-auto p-8 w-full sm:max-w-4xl flex flex-col items-center gap-16">
+    <div className="mx-auto p-4 sm:p-8 w-full sm:max-w-5xl flex flex-col items-center gap-8 sm:gap-12">
       <div className="flex flex-col gap-4 w-full">
-        <h1 className="text-px-30 font-polished leading-tight">
+        <h1 className="text-px-40 font-polished leading-tight">
           Hi! My name is Cammy.
         </h1>
         <h2 className="text-px-20 font-polished leading-tight">I am a front-end dev who dabbles in full-stack.</h2>
       </div>
-      <div className="flex flex-col md:flex-row items-start gap-12 md:gap-20 w-full">
-        <div className="w-full flex flex-col items-start gap-4 text-left">
-          <p className="text-md">
-            I have been building websites since Myspace and Neopets were the biggest thing. I have always had a passion for front-end development and UI/UX.
-          </p>
-          <p className="text-md">
-            I have built a lot of websites for various clients, from state-funded transportation authorities, country clubs, universities and then even small community based organizations. At the end of the day though, I have a passion for creating fun and unique websites, especially ones that are useful to others.
-          </p>
-          <p className="text-md">
-            My favorite website that I have built is <Link className="inline-flex items-center underline" href="https://polisheddex.app">PolishedDex <ArrowUpRight className="inline size-5" /></Link>, which is a data-mined Pokédex for the ROM Hack Polished Crystal. This project originally was done as a way to not have to review data files manually and rather wanting to try putting it into a auto generated human readable format which would allow for generating quick updates when the source is updated. It was also a neat way for me try some prompt-engineering!
-          </p>
-        </div>
-        <div className="flex flex-col gap-12 w-full">
-          <div className="rounded-lg overflow-hidden border border-lime-800 max-w-[300px] w-full">
+      <div className="flex flex-col md:flex-row items-start gap-8 md:gap-12 w-full">
+        <div className="w-full text-left">
+          <div className="rounded-lg overflow-hidden border border-lime-800 w-full sm:max-w-1/3 float-left mr-4">
             <div
-              className="relative flex-1 w-full bg-white aspect-square max-w-[300px] flex flex-col rendering-pixelated specimen-dots"
+              className="relative flex-1 w-full bg-white aspect-square max-w-full flex flex-col rendering-pixelated specimen-dots"
             >
               <div
                 className="font-polished text-black smooth-never text-px leading-none border-black bg-white border-b p-1 ring-1 ring-white flex justify-between space-x-2 px-2"
               >
                 <div className="flex justify-between space-x-3">
-                  <span>Pokedex</span>
+                  {/* <span>Pokedex</span> */}
                   {/* <span>PKMN</span> */}
-                  <span>Bag</span>
-                  <span>Options</span>
+                  {/* <span>Bag</span> */}
+                  {/* <span>Options</span> */}
                   {/* <span>Save</span> */}
                 </div>
                 <div className="flex justify-between space-x-3">
@@ -45,7 +34,7 @@ export default function Home() {
               </div>
               <div className="relative flex-1 z-0">
                 <div
-                  className="absolute text-black smooth-never text-px border-black bg-white border overflow-hidden rounded-sm block ring-1 ring-white font-polished bottom-2 right-2 z-10"
+                  className="absolute text-black smooth-never text-px border-black bg-white border overflow-hidden rounded-sm block ring-1 ring-white font-polished bottom-2 right-2 z-10 aspect-square w-3/5"
                 >
                   <div className="handle p-1 border-b border-black relative h-4">
                     <div
@@ -57,20 +46,20 @@ export default function Home() {
                     <div
                       className="h-2 w-2 rounded-sm border border-black absolute left-7 top-1"
                     ></div>
-                    <div className="absolute left-12 top-0">cammy.gif</div>
+                    {/* <div className="absolute left-12 top-0">cammy.gif</div> */}
                   </div>
                   <img
                     alt="pokemon trainer surrounded by their pokemon on top of a hill looking at the forest"
                     loading="lazy"
                     decoding="async"
                     data-nimg="1"
-                    className="block relative w-[170px] px-2 pt-2 rendering-pixelated object-contain bg-[#F8F8F8]"
+                    className="block relative w-full px-2 pt-2 rendering-pixelated object-contain bg-[#F8F8F8]"
                     src="/images/pikachu.webp"
                     style={{ color: 'transparent' }}
                   />
                 </div>
                 <div
-                  className="absolute text-black smooth-never text-px border-black bg-white border overflow-hidden rounded-sm block ring-1 ring-white font-polished w-40 sm:w-40 h-40 sm:h-40 top-2 left-2 z-0"
+                  className="absolute text-black smooth-never text-px border-black bg-white border overflow-hidden rounded-sm block ring-1 ring-white font-polished aspect-square w-3/5 top-2 left-2 z-0"
                 >
                   <div className="handle  p-1 border-b border-black relative h-4">
                     <div
@@ -82,21 +71,28 @@ export default function Home() {
                     <div
                       className="h-2 w-2 rounded-sm border border-black absolute left-7 top-1"
                     ></div>
-                    <div className="absolute left-12 top-0">intro.txt</div>
+                    {/* <div className="absolute left-12 top-0">intro.txt</div> */}
                   </div>
                   <div
                     data-hj-allow="true"
                     className="p-1 border-none text-black smooth-never text-px text-left resize-none leading-none w-full h-full bg-white no-scrollbar"
                   >
-                    hi :)
-                    <br />
-                    <br />
-                    my name is cammy, welcome to my website!
+                    hi!
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <p className="text-sm sm:text-md mb-4">
+            I have been building websites since Myspace and Neopets were the biggest thing. I have always had a passion for front-end development and UI/UX.
+          </p>
+          <p className="text-sm sm:text-md mb-4">
+            I have built a lot of websites for various clients, from state-funded transportation authorities, country clubs, universities and then even small community based organizations. At the end of the day though, I have a passion for creating fun and unique websites, especially ones that are useful to others.
+          </p>
+          <p className="text-sm sm:text-md mb-4">
+            My favorite website that I have built is <Link className="inline-flex items-center underline" href="https://polisheddex.app">PolishedDex <ArrowUpRight className="inline size-5" /></Link>, which is a data-mined Pokédex for the ROM Hack Polished Crystal. This project originally was done as a way to not have to review data files manually and rather wanting to try putting it into a auto generated human readable format which would allow for generating quick updates when the source is updated. It was also a neat way for me try some prompt-engineering!
+          </p>
+
           <div>
             <p>
               <Link href={'https://www.linkedin.com/in/caomicc/'}
@@ -121,7 +117,8 @@ export default function Home() {
             </p>
           </div>
         </div>
+        <iframe id='kofiframe' src='https://ko-fi.com/caomicc/?hidefeed=true&widget=true&embed=true&preview=true' className="border-none w-full md:w-auto md:min-w-[320px]" height={712} title='caomicc'></iframe>
       </div>
-    </div>
+    </div >
   );
 }
